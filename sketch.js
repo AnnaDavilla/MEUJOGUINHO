1fda96130aa1a24a90c1f7870e1650dfded1b706
AnnaDavilla <110867038+AnnaDavilla@users.noreply.github.com>
2023-01-02 23:40:33 +0000

    concluindo progeto

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-
+// criação de variaveis globais
 var menina, meninaImg
 
 var floresta, florestaImg
@@ -16,11 +16,11 @@ var PLAY = 1;
 var END = 0;
 var gameState = PLAY;
 
-
+// carregando as imagens 
 function preload(){
     meninaImg = loadImage("menina.png")
     florestaImg = loadImage("floresta1.png")
-    chaoImg = loadImage("chão.jpg")
+    chaoImg = loadImage("chao.jpg")
 
     //carregar imagem dos obstaculos 1, 2, 3 ;
     obstaculo1 = loadImage("cacto1.png")
@@ -30,65 +30,76 @@ function preload(){
 }
 
 function setup() {
- createCanvas(1000,600)
+  // codigo para estabelecer o tamanho da tela 
+  createCanvas(1000,600)
 
+  // criação do cenario 
+  floresta = createSprite(300,300);
+  floresta.addImage("floresta",florestaImg);
+  floresta.velocityX= -5;
+  floresta.scale =0.8
 
-    floresta = createSprite(300,300);
-    floresta.addImage("floresta",florestaImg);
-    floresta.velocityX= -5;
-    floresta.scale =0.8
+  // criação do personagem 
+  menina = createSprite(179,530,20,20)
+  menina.addImage("menina",meninaImg)
+  menina.scale = 0.4;
 
-    menina = createSprite(179,530,20,20)
-    menina.addImage("menina",meninaImg)
-    menina.scale = 0.4;
+  //criação do chão 
+  chao = createSprite(500,585)
+  chao.addImage("chao", chaoImg)
+  chao.velocityX = -5
+  chao.scale = 0.5
 
-    chao = createSprite(500,585)
-    chao.addImage("chao", chaoImg)
-    chao.velocityX = -5
-    chao.scale = 0.5
+  // criação do grupo de obstaculos 
+  obstaculogrupo = new Group();
 
+  // criação do chão invisivel
+  invisibleGround = createSprite(500,600,1000,10);
+  invisibleGround.visible = false;
 
-    obstaculogrupo = new Group();
-
-    invisibleGround = createSprite(500,600,1000,10);
-    invisibleGround.visible = false;
-
-}
+ }
 
 function draw() {
-
+    // cor do fundo 
     background("pink")
-     gerarObstaculo();
-      
+
+    // chamando a função para gerar obstaculos 
+    gerarObstaculo();
+
+    //acumulando pontuação de acordo com a quantidade de quadros   
     score = score + Math.round(getFrameRate()/60);
 
-
+    // criando fundo infinito da floresta 
     if (floresta.x < 300){
         floresta.x = 800
-      }
-      if(chao.x <300){
-        chao.x = 600
-      }
-
-
-      if(keyDown("space")&& menina.y >= 100) {
-        menina.velocityY = -12;
-       
     }
-      
 
+    // chão infinito 
+    if(chao.x <300){
+      chao.x = 600
+    }
+    // se a tecla espaço for precionada a menina vai pular 
+    if(keyDown("space")&& menina.y >= 100) {
+      menina.velocityY = -12;
+    }
+    //gravidade para menina 
     menina.velocityY = menina.velocityY +1;
 
+    // menina colidir com o chão invisivel 
     menina.collide(invisibleGround)
 
+    // verificar que obstaculos toca na menina 
+    if(obstaculogrupo.isTouching(menina)){
+      obstaculogrupo.setVelocityXEach(0)
+      floresta.velocityX = 0;
+    }
+    // desenhando os sprites 
     drawSprites()
     textSize(20)
     fill("red")
     text("desenvolvido por : Anna Dávilla ",50,40)
     text("pontuação: "+score,800,40)
 }
-      
-           
 
 function gerarObstaculo(){
     if (frameCount % 90 === 0){
